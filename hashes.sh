43dbfa1a1b9f156d24f04b336880e881fad05db0
Jakob Kirsch <jakob.kirsch@web.de>
2024-11-04 19:09:05 +0100

    add hash script

---
+++ hashes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE=$(pwd)
+
+for BRANCH in kernelpanicroom dalaptop; do
+    echo -ne \(list \"$BRANCH\"\ 
+
+    if [ -d /tmp/checkout ]; then rm -fr /tmp/checkout; fi
+
+    git clone $BASE -b $BRANCH /tmp/checkout 2> /dev/null
+    pushd /tmp/checkout > /dev/null
+
+    echo -ne \"$(git rev-parse HEAD)\"\ 
+    echo -ne \"$(guix hash -rx .)\"\)
+    echo
+
+    popd > /dev/null
+done
